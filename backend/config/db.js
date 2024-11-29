6a3cb8fe2dd9ea421e363aecf44be50960271c33
Evan Perry <eperry2688@gmail.com>
2024-11-29 16:09:51 -0700

    Fix typo in Sequelize database_URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_UR, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false
 })
